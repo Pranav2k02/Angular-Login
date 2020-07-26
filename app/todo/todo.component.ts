fd02565a328bcb6a440a7f28672d3d7e62102044
pranav2002 <pranavvenugopalan2002@gmail.com>
2020-07-26 19:04:12 +0530

    Login

---
+++ app/todo/todo.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RequiredValidator } from '@angular/forms';
+
+@Component({
+  selector: 'todo',
+  templateUrl: 'todo.component.html',
+  styleUrls: ['todo.component.css']
+})
+export class TodoComponent {
+
+  constructor() { }
+
+
+}
